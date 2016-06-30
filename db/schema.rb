c642279700c920db787cac670fec75d5d949c7a8
David Whitaker <damwhit@gmail.com>
2016-06-30 09:07:47 -0600

    add user model and table
    needed to add custom initializer as groves was being singularized to grofe.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160630145353) do
+ActiveRecord::Schema.define(version: 20160630150659) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,5 +31,20 @@ ActiveRecord::Schema.define(version: 20160630145353) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.integer  "school_id"
+    t.integer  "grove_id"
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  add_index "users", ["grove_id"], name: "index_users_on_grove_id", using: :btree
+  add_index "users", ["school_id"], name: "index_users_on_school_id", using: :btree
+
   add_foreign_key "groves", "schools"
+  add_foreign_key "users", "groves"
+  add_foreign_key "users", "schools"
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,4 +1,9 @@
 FactoryGirl.define do
+  factory :user do
+    name "JJ"
+    email "Letest"
+    password_digest "password"
+  end
   factory :grove do
     name "Grove 1"
   end
