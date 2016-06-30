6e40814ecbf9636a393cf04d7583d82fee94b0cb
David Whitaker <damwhit@gmail.com>
2016-06-30 16:47:21 -0600

    pull login page object into its own file

--- spec/integration/user_can_log_in_with_email_and_password_spec.rb
+++ spec/integration/user_can_log_in_with_email_and_password_spec.rb
@@ -1,21 +1,4 @@
 require 'rails_helper'
-
-class LoginPage
-  include Capybara::DSL
-
-  def visit_page
-    visit '/'
-    click_on 'Login'
-    self
-  end
-
-  def login(user)
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: 'password'
-    click_on 'Log In'
-  end
-end
-
 RSpec.feature 'User can login' do
   let(:user) { create(:user) }
   let(:login_page) { LoginPage.new }
@@ -23,7 +6,6 @@ RSpec.feature 'User can login' do
   scenario "User can login with email and password" do
 
     login_page.visit_page.login(user)
-
     expect(current_path).to eq(root_path)
     expect(page).to have_content("Welcome, #{user.name}")
     expect(page).to have_content("Signed in successfully.")
