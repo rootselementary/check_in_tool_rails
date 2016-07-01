376e9309d72bc5737ded7ec099330d953fa2b8c6
David Whitaker <damwhit@gmail.com>
2016-07-01 14:22:04 -0600

    student and teachers are able to login

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,8 @@
 FactoryGirl.define do
   factory :role do
-    name "MyString"
+    name "student"
   end
+
   factory :user do
     sequence(:name) { |n| "JJ Letest#{n}" }
     sequence(:email) { |n| "letest#{n}@example.com" }
@@ -11,6 +12,7 @@ FactoryGirl.define do
   factory :grove do
     name "Grove 1"
   end
+
   factory :school do
     name "Roots Elementary"
   end
--- spec/integration/authentication/sign_in_spec.rb
+++ spec/integration/authentication/sign_in_spec.rb
@@ -2,20 +2,44 @@ require 'rails_helper'
 include Pages::Authentication
 
 RSpec.feature 'User can login' do
-  let(:user) { create :user }
+  let(:role1) { create :role }
+  let(:role2) { create :role, name: 'teacher' }
 
-  describe "with valid credentials" do
-    scenario "User can login with email and password" do
-      login(user)
+  let(:student) { create :user }
+  let(:teacher) { create :user }
+
+  describe "teacher with valid credentials" do
+    scenario "teacher can login with email and password" do
+      teacher.role = role2
+      login(teacher)
       expect(current_path).to eq(root_path)
-      expect(page).to have_content("Welcome, #{user.email}")
+      expect(page).to have_content("Welcome, #{teacher.email}")
       expect(page).to have_content("Signed in successfully.")
     end
   end
 
-  describe "with invalid credentials" do
+  describe "teacher with invalid credentials" do
     it "provides a kind error message" do
-      login(user, "passwordXXX")
+      teacher.role = role2
+      login(teacher, "passwordXXX")
+      expect(page).to have_content("Invalid Email or password friend.")
+    end
+  end
+
+  describe "student with valid credentials" do
+    scenario "student can login with email and password" do
+      student.role = role1
+      login(student)
+      expect(current_path).to eq(root_path)
+      expect(page).to have_content("Welcome, #{student.email}")
+      expect(page).to have_content("Signed in successfully.")
+    end
+  end
+
+  describe "student with invalid credentials" do
+    it "provides a kind error message" do
+      student.role = role1
+      login(student, "passwordXXX")
       expect(page).to have_content("Invalid Email or password friend.")
     end
   end
