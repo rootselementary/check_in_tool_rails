d5c3b67fd4524c43a8953c97428395a121422cf5
Brian Rippeto <rippeto@gmail.com>
2016-07-05 22:30:23 -0600

    factories for users now utilize traits and inheritance

--- spec/models/teacher_spec.rb
+++ spec/models/teacher_spec.rb
@@ -3,4 +3,10 @@ require 'rails_helper'
 RSpec.describe Teacher, type: :model do
   it { should have_many(:user_roles) }
   it { should have_many(:roles) }
+
+  # TODO: this is just a stub spec to show how to use traits.
+  it 'can haz roles' do
+    teach = create(:teacher, :admin)
+    expect(teach.roles.size).to eq(1)
+  end
 end
