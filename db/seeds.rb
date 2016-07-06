89ee0f2e43c204597ce7c8f7fd86f07567f2b65d
Erinna Chen <erinna.chen@gmail.com>
2016-07-06 09:42:53 -0600

    Add sample users to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
 ['teacher', 'student', 'admin'].each do |role|
   Role.find_or_create_by({name: role})
 end
+
+Teacher.create!(email: "teacher@example.com", password: "password")
+Student.create!(email: "student@example.com", password: "password")
