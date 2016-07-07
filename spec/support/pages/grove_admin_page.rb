2952b974f343e8be323267060a00f22816dfd87d
Jed Schneider <jed.schneider@gmail.com>
2016-07-07 10:02:03 -0700

    adds simple form to project

--- spec/support/pages/grove_admin_page.rb
+++ spec/support/pages/grove_admin_page.rb
@@ -2,7 +2,7 @@ module Pages
   class GroveAdminPage < Page
     def create_new_grove(name="Ponderosa")
       page.has_content? "Create A New Grove"
-      page.fill_in 'grove[name]', with: name
+      page.fill_in "Name", with: name
       page.click_on "Save"
       page.has_content? "Grove Saved"
     end
