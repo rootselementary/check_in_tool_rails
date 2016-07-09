0ea64b58af0f545ccc505e077c5c932c46513193
Nicholas Dorans <Nicky_Bobby@icloud.com>
2016-07-09 14:44:54 -0600

    Adds Google oauth to login
    - Google login is now scoped to within the rootselementary.org domain
    - Documented Stuff that comes back from Google for two different cases

--- app/models/users/user.rb
+++ app/models/users/user.rb
@@ -7,13 +7,22 @@ class User < ActiveRecord::Base
 
   def self.from_omniauth(access_token)
     data = access_token.info
-    user = User.where(email: data["email"]).first
-    unless user
-      user = User.create(name: data["name"],
-                         email: data["email"],
-                         password: Devise.friendly_token[0,20]
-                         )
+
+    if within_roots?(data)
+      user = User.where(email: data["email"]).first
+      unless user
+        user = User.create(name: data["name"],
+                           email: data["email"],
+                           password: Devise.friendly_token[0,20]
+                           )
+      end
+      user
+    else
+      user = User.new
     end
-    user
+  end
+
+  def self.within_roots?(data)
+    data["email"].include?("rootselementary.org")
   end
 end
---
+++ spec/integration/authentication/sign_in_with_good_google_spec.rb
@@ -0,0 +1,53 @@
+require 'rails_helper'
+include Pages::Authentication
+
+RSpec.feature 'User can login' do
+  let(:student) { create :student }
+  let(:teacher) { create :teacher }
+
+  before do
+    OmniAuth.config.test_mode = true
+
+    OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
+      provider: "Google",
+      uid: "11111111",
+      info: {
+        name: "jj_letest",
+        email: "letest@rootselementary.org",
+        first_name: "JJ",
+        last_name: "Letest",
+        image: ""
+      },
+      credentials: {
+        token: "token",
+        refresh_token: "another_token",
+        expires_at: 1354920555,
+        expires: true
+      },
+      extra: {
+        id_token: 1000.times.map { "string" }.join, # this huge chunk is used to test for CookieOverflow exception
+        raw_info: OmniAuth::AuthHash.new(
+          email: "test@example.com",
+          email_verified:"true",
+          kind:"plus#personOpenIdConnect",
+          name:"Test Person",
+        )
+      }
+    })
+  end
+
+  describe "as a google user within the Roots org" do
+    scenario "Teacher within Roots org with correct credentials logs in" do
+      login_with_google(teacher)
+
+      expect(page).to have_content("Welcome, letest@rootselementary.org")
+      expect(page).to have_content("Not you?")
+
+    end
+  end
+
+  after do
+    OmniAuth.config.test_mode = false
+  end
+
+end
