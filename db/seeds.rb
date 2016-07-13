faa6d0a5e6ba41c5a7e3e9b63f211a245f01efda
Erinna Chen <erinna.chen@gmail.com>
2016-07-13 16:21:16 -0600

    Add another student to the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
 grove = Grove.create!(name: "Aspen")
 Teacher.create!(name: "Jane Teacher", email: "teacher@rootselementary.org", password: "password", grove: grove)
-Student.create!(name: "JJ Letest", email: "student@rootselementary.org", password: "password", grove: grove)
+Student.create!(name: "JJ Letest", email: "jj.letest@rootselementary.org", password: "password", grove: grove)
+Student.create!(name: "Jill Letest", email: "jill.letest@rootselementary.org", password: "password", grove: grove)
 
 admin = Teacher.create!(name: "Daisy Admin", email: "admin@rootselementary.org", password: "password", grove: grove)
 role = Role.create(name: 'admin')
