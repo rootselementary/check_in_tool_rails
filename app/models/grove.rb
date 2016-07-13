03ef97a5ceb3e128410649d2902b0fe910765d57
Erinna Chen <erinna.chen@gmail.com>
2016-07-13 15:54:32 -0600

    Add relations to grove

--- app/models/grove.rb
+++ app/models/grove.rb
@@ -2,6 +2,7 @@ class Grove < ActiveRecord::Base
   belongs_to :school
   has_many :users, dependent: :nullify
   has_many :students
+  has_many :teachers
 
   validates :name, presence: true, uniqueness: true
 end
--- spec/models/grove_spec.rb
+++ spec/models/grove_spec.rb
@@ -3,4 +3,6 @@ require 'rails_helper'
 RSpec.describe Grove, type: :model do
   it { should belong_to(:school) }
   it { should have_many(:users) }
+  it { should have_many(:students) }
+  it { should have_many(:teachers) }
 end
