4e5e55c5ee7c3b929cd11e80cb71f1d2e22e76c2
Jbern16 <jbern16@gmail.com>
2016-07-29 16:17:15 -0600

    fix broken tests

--- spec/requests/edit_playlist_spec.rb
+++ spec/requests/edit_playlist_spec.rb
@@ -4,24 +4,19 @@ describe "PATCH api/v1/admin/playlist_activities/:id" do
   let(:grove) { create(:grove_with_resources) }
   let(:student) { grove.students.first } 
   let(:playlist_activity) { student.playlist_activities.first }
-  let(:playlist_activity2) { student.playlist_activities.last }
 
   it "can update the position priority of the activities for a specific student " do 
-    data = { activities: { (playlist_activity.id.to_s) => "0",
-                          (playlist_activity2.id.to_s) => "1", 
-                        },
+    data = { activities: { (playlist_activity.id.to_s) => "1" },
              student_id: student.id
             }
                                     
     expect(playlist_activity.position).to eq nil
-    expect(playlist_activity2.position).to eq nil
-
+  
     patch api_v1_admin_playlist_activity_path(1), { data: data }
 
     expect(response.status).to eq 204
 
-    expect(student.playlist_activities.first.position).to eq 0
-    expect(student.playlist_activities.last.position).to eq 1
+    expect(student.playlist_activities.first.position).to eq 1
   end  
 end 
 
