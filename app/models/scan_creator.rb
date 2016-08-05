a96446b4eaabb8dd4cea948431cfd773bfe212f6
Erinna Chen <erinna.chen@gmail.com>
2016-08-05 13:43:48 -0600

    Add event to scan creation

--- app/models/scan_creator.rb
+++ app/models/scan_creator.rb
@@ -1,8 +1,9 @@
 class ScanCreator
   def self.create(student, scan_params)
     Scan.create!(student: student,
-                location_id: scan_params[:location_id],
-                timestamp: Time.now,
-                correct: scan_params[:location_id] == scan_params[:scanned_data])
+                 event_id: student.current_event.id,
+                 location_id: scan_params[:location_id],
+                 timestamp: Time.now,
+                 correct: scan_params[:location_id] == scan_params[:scanned_data])
   end
 end
--- spec/controllers/compass_controller_spec.rb
+++ spec/controllers/compass_controller_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe CompassController, type: :controller do
     context "incorrect location for scanning" do
       it "creates a new scan with incorrect status and redirects to compass" do
         student = create(:student)
+        create(:event, user_id: student.id)
         location1 = create(:location)
         location2 = create(:location)
         allow(request.env['warden']).to receive(:authenticate!).and_return(student)
@@ -23,6 +24,7 @@ RSpec.describe CompassController, type: :controller do
       context "correct location for scanning" do
         it "creates a new scan with correct status and redirects to compass" do
           student = create(:student)
+          create(:event, user_id: student.id)
           location1 = create(:location)
           allow(request.env['warden']).to receive(:authenticate!).and_return(student)
           allow(controller).to receive(:current_user).and_return(student)
