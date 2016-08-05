b8421160f4769a0fb8907c711189fe2f710f881a
Erinna Chen <erinna.chen@gmail.com>
2016-08-05 14:24:23 -0600

    Remove update repeat from grove monitor controller

--- app/controllers/admin/grove_monitor_controller.rb
+++ app/controllers/admin/grove_monitor_controller.rb
@@ -18,19 +18,7 @@ class Admin::GroveMonitorController < ApplicationController
   def update
     authorize(:grove_monitor, :index?)
     student = Student.find(params[:id])
-    if student.at_school
-      student.update(student_params)
-    else
-      student.update(student_params)
-    end
-    redirect_to admin_grove_monitor_all_path
-  end
-
-  def update
-    authorize(:grove_monitor, :index?)
-    student = Student.find(params[:id])
     student.update(student_params)
-
     redirect_to admin_grove_monitor_all_path
   end
 
