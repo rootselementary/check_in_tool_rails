d7a181a05c0021cac311378cd9a1fdbd03d2b00b
Matt Pindell <pindell.m@gmail.com>
2016-08-16 17:06:07 -0400

    Update ActivitiesController to use current_grove method

--- app/controllers/admin/activities_controller.rb
+++ app/controllers/admin/activities_controller.rb
@@ -1,6 +1,6 @@
 class Admin::ActivitiesController < Admin::ResourceController
   def new
-    @locations = Location.where(grove_id: current_user.grove_id)
+    @locations = Location.where(grove_id: current_grove.id)
     super
   end
 
@@ -11,7 +11,7 @@ class Admin::ActivitiesController < Admin::ResourceController
   end
 
   def edit
-    @locations = Location.where(grove_id: current_user.grove_id)
+    @locations = Location.where(grove_id: current_grove.id)
     super
   end
 
