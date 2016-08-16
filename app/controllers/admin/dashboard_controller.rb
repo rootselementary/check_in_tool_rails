f77301f7577660b3307e6973ae3e8dbd599ccb6d
Matt Pindell <pindell.m@gmail.com>
2016-08-16 16:57:52 -0400

    Refactor to use simple_form_for

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class Admin::DashboardController < ApplicationController
   def show
     authorize(:admin_dashboard, :show?)
-    @groves = Grove.where.not(name: current_user.grove.name)
+    @groves = Grove.all
   end
 end
