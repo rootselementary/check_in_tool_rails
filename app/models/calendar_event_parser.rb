980d1dcd4b101e693b061113fb5da5dc80df96de
Jed Schneider <jed.schneider@gmail.com>
2016-08-17 11:38:34 -0700

    use the right method

--- app/models/calendar_event_parser.rb
+++ app/models/calendar_event_parser.rb
@@ -6,7 +6,7 @@ module CalendarEventParser
        end_time: Time.zone.parse(event.end_time),
        title: event.title,
       #  activity_id: Activity.find_by(name: event.title).id,
-       location_id: Location.find_by(name: event.location).id,
+       location_id: Location.find_by_location(event.location).id,
        creator: event.creator_name,
       #  creator_id: User.find_by(email: event.creator_name).id if event.creator_name,
        metadata: event.to_json
