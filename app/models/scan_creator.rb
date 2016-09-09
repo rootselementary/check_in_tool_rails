40e2fc1ca7e28191cf8db17d86490a39cbe9aa54
Jbern16 <jbern16@gmail.com>
2016-09-09 14:27:04 -0400

    adding stucture

--- app/models/scan_creator.rb
+++ app/models/scan_creator.rb
@@ -1,9 +1,12 @@
 class ScanCreator
   def self.create(student, scan_params)
-    Scan.create!(student: student,
+    scan = Scan.new
+    if scan.save!(student: student,
                  location_id: scan_params[:scanned_data],
                  timestamp: Time.now,
                  correct: scan_params[:location_id] == scan_params[:scanned_data],
                  activity_id: student.current_event.activity_id)
+       #broadcast message here
+    end  
   end
 end
